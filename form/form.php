bc3d9b48d6b540a3fc1d1587414c8230697f0b18
Ezequiel Salina <eze2334@gmail.com>
2022-07-07 21:07:50 -0300

    actualizando proyecto

---
+++ form/form.php
@@ -0,0 +1,21 @@
+<?php
+$nombre=$_POST{"name"};
+$mail=$_POST{"email"};
+$telefono=$_POST{"telefono"};
+$mensaje=$_POST{"mensaje"};
+
+$header .="Content-type : text/plain";
+
+$mensaje="Este mensaje fue enviado por" .$nombre . ",\r\n";
+$mensaje.="Su email es: " . $mail . "\r\n";
+$mensaje.="Su telefono es : " $telefono . "\r\n";
+$mensaje.="Su mensaje es : " $mensaje . "\r\n";
+
+$para="eze2334@gmail.com";
+$asunto="Este mail fue enviado desde la web";
+
+mail($para, $asunto, utf8_decode($mensaje), $header);
+
+header{"location://pages/exito.html"};
+
+?>
